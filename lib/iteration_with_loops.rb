afb26498c2a66749fd5cf305cc98fe4ca57c11e6
Lindsay Pierce <lindsay.pierce@yale.edu>
2020-05-20 21:40:51 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -9,6 +9,8 @@ def find_even_values(src)
       if src[row_index][element_index] % 2 == 0
         puts src[row_index][element_index]
       end
+      element_index += 1
     end
+    row_index += 1
   end
 end
